8df7f03ec743593bf91d713e3cb4b3522038e9b2
Andres-Pereira <andresgpereira@gmail.com>
2023-03-10 23:26:46 -0500

    solución a los ejercicios de js

---
+++ hello.js
@@ -0,0 +1,8 @@
+function hola(nombre){
+frase = "Hola" + nombre + "!"
+return (frase);
+}
+
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
---
+++ sum.js
@@ -0,0 +1,12 @@
+function suma (num){
+    var suma1, i
+    suma1=0
+    for (let i = 1; i <= num; i++) {
+        suma1 = suma1 + i;
+        }
+        return suma1
+}
+
+console.log(suma(4)) // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)) // 55
+console.log(suma(15)) // 120
